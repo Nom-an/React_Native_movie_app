245d8ba57f71871789eebdb07fd08a72698b21b9
Noman-Al <117844247+Noman-Al@users.noreply.github.com>
2026-07-01 23:52:46 +0600

    fixed navigation and style

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,25 +2,58 @@ import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
 import { Tabs } from "expo-router";
 import React from "react";
-import { Image, ImageBackground, Text } from "react-native";
+import { Image, ImageBackground, Text, View } from "react-native";
 
 const TabIcon = ({ focused, icon, title }: any) => {
-  return (
-    <ImageBackground
-      source={images.highlight}
-      className="flex flex-row w-full flex-1 min-w-[112px] mt-4 min-h-12 justify-center items-center rounded-full overflow-hidden   "
-    >
-      <Image source={icon} tintColor="#151312" className="size-5" />
-      <Text className="ml-2 font-semibold text-secondary text-base ">
-        {title}
-      </Text>
-    </ImageBackground>
-  );
+  if (focused) {
+    return (
+      <ImageBackground
+        source={images.highlight}
+        className="flex flex-row w-full flex-1 min-w-[112px] mt-4 min-h-16 justify-center items-center rounded-full overflow-hidden   "
+      >
+        <Image source={icon} tintColor="#151312" className="size-5" />
+        <Text className="ml-2 font-semibold text-secondary text-base ">
+          {title}
+        </Text>
+      </ImageBackground>
+    );
+  } else {
+    return (
+      <View className="size-full justify-center items-center mt-4">
+        <Image source={icon} tintColor="#A8B5DB" className="size-5" />
+      </View>
+    );
+  }
 };
 
 const _layout = () => {
   return (
-    <Tabs>
+    <Tabs
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarItemStyle: {
+          width: "100%",
+          height: "100%",
+          justifyContent: "center",
+          alignItems: "center",
+        },
+        tabBarStyle: {
+          backgroundColor: '#0f0d23',
+          borderRadius:50,
+          marginHorizontal:20,
+          marginBottom:36,
+          height:52,
+          position:"absolute",
+          overflow:"hidden",
+          borderWidth:1,
+          borderColor:'#0f0d23',
+
+
+
+
+        },
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
