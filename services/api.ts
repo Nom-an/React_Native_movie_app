5bc119286ba1f0f47828ae3ec1b5e2e12ea09d52
Noman-Al <nomanvi123@gmail.com>
2026-07-15 22:53:48 +0600

    api

--- services/api.ts
+++ services/api.ts
@@ -1,10 +1,15 @@
-// const url = 'https://api.themoviedb.org/3/keyword/keyword_id/movies?include_adult=false&language=en-US&page=1';
-// const options = {
-//   method: 'GET',
-//   headers: {accept: 'application/json', Authorization: 'Bearer ••••'}
-// };
+export const TMDB_CONFIG={
+    BASE_URL:'https://api.themoviedb.org/3',
+    API_KEY:process.env.EXPO_PUBLIC_MOVIE_API_KEY,
+    headers:{
+        accept:'application/json',
+        Authorization:`Bearer ${process.env.EXPO_PUBLIC_MOVIE_API_KEY}`
+    }
+}
 
-// fetch(url, options)
-//   .then(res => res.json())
-//   .then(json => console.log(json))
-//   .catch(err => console.error(err));
+export const fetchMovie = async({query} :{ query :string})=>{
+
+}
+
+
+// /discover/movie
